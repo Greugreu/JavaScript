711e28587da3db3af48e3828234ef8688b6cf1a2
Victor Clarke <fenris.fitz@hotmail.fr>
2019-02-04 11:57:38 +0100

    basic function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,13 @@
 var message = prompt('veuillez saisir une phrase');
 console.log(message);
+var reponse ="";
+if(message < 0){
+  reponse = 'nombre négatif';
+}
+else if (message > 0){
+  reponse = 'Nombre positif'
+}
+else {
+  reponse = 'Null'
+}
+alert(reponse);
