88ef02e7240b1918b4aeed6120cadb49d81798b0
Victor Clarke <fenris.fitz@hotmail.fr>
2019-02-04 15:03:59 +0100

    exercice compteur

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,11 @@
-var resultat ="<ul>";
-for (var i=0; i <=100; i++) {
-  resultat=resultat+"<li>"+i+"</li>";
+var resultat="<ul>";
+var compteur=0;
+for (var i=-12728; i <=16856; i+=17) {
+  if (i%23==0){
+    resultat+="<li>"+i+"</li>";
+    compteur++;
+  }
 }
 resultat=resultat+"</ul>";
 document.write(resultat);
+alert(compteur);
